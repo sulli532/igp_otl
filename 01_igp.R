96365f6f31179019d74308cf094b2a4afbda281a
William Sullivan <sulli532@msu.edu>
2022-02-01 13:01:38 -0500

    intro

---
+++ 01_igp.R
@@ -0,0 +1,10 @@
+# *******************************************************************************
+# Title: 01_igp.R
+# Created by: William Sullivan
+# Created on: 9/21/2020
+#                     *Purpose: This script demonstrates how the International
+#                               Grade Placement index (OTL) is created.
+# *******************************************************************************
+# 
+# The 2012 PISA Survey asks students a number of questions with respect to
+# their experience with formal mathematics instruction.
